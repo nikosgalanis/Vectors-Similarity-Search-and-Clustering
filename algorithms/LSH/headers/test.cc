9912e19e3e689fbc7808985dbcbceca11d4cdec6
Sofo <46571975+SofoSt@users.noreply.github.com>
2020-10-09 18:41:01 +0300

    AmplifiedHashFunctiton

--- algorithms/LSH/headers/test.cc
+++ algorithms/LSH/headers/test.cc
@@ -9,7 +9,14 @@ int main(void) {
     vector<float> x = {1.0,2.2,3.4,4.5};
     vector<float> y = {6000.3,3023.3,9212.4,1544.2};
 
-    lsh::HashFunction instant(4, 5.0, 1000.0, (pow(2,16) - 5), pow(2,8));
+    // lsh::HashFunction instant(4, 5.0, 1000.0, (pow(2,16) - 5), pow(2,8));
 
-    cout << instant.Hash(x) << endl;
+    // cout << instant.Hash(x) << endl;
+
+    lsh::AmplifiedHashFunction ampl(4, 5.0, 1000.0, (pow(2,16) - 5), pow(2,8), 4);
+
+    cout << ampl.Hash(x) << endl;
+    //TODO what should it print ? bucket ?
+    return 0;
+
 }
