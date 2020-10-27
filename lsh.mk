aa927a5664810057af59b3b892079fa83593910a
Sofo <46571975+SofoSt@users.noreply.github.com>
2020-10-27 21:46:25 +0200

    Makefile update

--- lsh.mk
+++ lsh.mk
@@ -1,22 +1,24 @@
+# Makefile for lsh target
 LSH_TARGET_EXEC = lsh
 
+# Directories
 LSH_BUILD_DIR = ./build/lsh
 LSH_SRC_DIRS = programs/common programs/LSH 
-
 LSH_INC_DIRS = algorithms/Search/LSH/headers/ algorithms/Search/BruteForce/headers/ algorithms/common/ programs/common/ utils/
-
+# Flags
 LSH_INC_FLAGS := $(addprefix -I,$(LSH_INC_DIRS))
-
+#Files
 LSH_SRCS := $(shell find $(LSH_SRC_DIRS) -name *.cc)
-
+LSH_HEADS:= $(shell find $(LSH_INC_DIRS) -name '*.h')
 LSH_OBJS := $(LSH_SRCS:%=$(LSH_BUILD_DIR)/%.o)
 LSH_DEPS := $(LSH_OBJS:.o=.d)
 
 
 $(LSH_BUILD_DIR)/$(LSH_TARGET_EXEC): $(LSH_OBJS)
-	$(CC) $(LSH_OBJS) -o $@
+	@echo Building lsh...
+	@$(CC) $(LSH_OBJS) -o $@
 
 # c source
-$(LSH_BUILD_DIR)/%.cc.o: %.cc
-	$(MKDIR_P) $(dir $@)
-	$(CC) -c $(CC_FLAGS) $< -o $@ -lm
+$(LSH_BUILD_DIR)/%.cc.o: %.cc $(LSH_HEADS)
+	@$(MKDIR_P) $(dir $@)
+	@$(CC) -c $(CC_FLAGS) $< -o $@ -lm
